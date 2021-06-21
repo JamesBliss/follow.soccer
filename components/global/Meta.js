9740ef4a3addbea87ee25930c6e9e56290159a3b
James Bliss <james.bliss@lush.co.uk>
2021-06-21 17:24:32 +0100

    adding social

--- components/global/Meta.js
+++ components/global/Meta.js
@@ -3,25 +3,60 @@ import Head from 'next/head';
 const Meta = () => (
   <Head>
     <title>What are football scores?</title>
-    <meta name="description" content='Find out what is going on next in football' />
-    <meta property='og:site_name' content='follow.soccer' />
-    <meta property='description' content='Just see the scores for today!' />
-    <meta property='twitter:site' content='@thejamesbliss' />
-    <meta property='twitter:card' content='summary_large_image' />
-    <meta property='twitter:title' content='follow.soccer?' />
-    <meta property='og:description' content='Just see the scores for today!' />
-    <meta property='og:title' content='follow.soccer?' />
-    <meta property='og:type' content='website' />
-    <meta property='og:url' content='https://follow.soccer' />
+    <meta name="description" content="Find out what is going on next in football" />
+    <meta property="og:site_name" content="follow.soccer" />
+    <meta property="description" content="Just see the scores for today!" />
+    <meta property="twitter:site" content="@thejamesbliss" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:title" content="follow.soccer?" />
+    <meta property="og:description" content="Just see the scores for today!" />
+    <meta property="og:title" content="follow.soccer?" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://follow.soccer" />
 
-    {/* Fix the below */}
-    <meta property='twitter:card' content='summary_large_image' />
-    <meta property='twitter:image' content='https://res.cloudinary.com/jamesbliss/image/upload/v1550847183/areliverpoolwinning/sharing/twitter.png' />
-    <meta property='og:image' content='https://res.cloudinary.com/jamesbliss/image/upload/v1550847183/areliverpoolwinning/sharing/facebook.png' />
-    <link rel="shortcut icon" href="https://res.cloudinary.com/jamesbliss/image/upload/v1547821777/areliverpoolwinning/logo/favicon.ico"></link>
-    <link rel="apple-touch-icon" sizes="180x180" href="https://res.cloudinary.com/jamesbliss/image/upload/v1547822257/areliverpoolwinning/logo/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="https://res.cloudinary.com/jamesbliss/image/upload/v1547822257/areliverpoolwinning/logo/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="https://res.cloudinary.com/jamesbliss/image/upload/v1547822257/areliverpoolwinning/logo/favicon-16x16.png" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta
+      property="twitter:image"
+      content="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/twitter.png"
+    />
+    <meta
+      property="og:image"
+      content="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/facebook.png"
+    />
+
+    <link
+      rel="manifest"
+      href="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/site.webmanifest"
+    />
+    <link
+      rel="mask-icon"
+      href="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/safari-pinned-tab.svg"
+      color="#000000"
+    />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="theme-color" content="#333333" />
+
+    <link
+      rel="shortcut icon"
+      href="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/favicon.ico"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="https://res.cloudinary.com/jamesbliss/image/upload/v1624292630/followsoccer/sharing/favicon-16x16.png"
+    />
   </Head>
 );
 
